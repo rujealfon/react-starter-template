a25a5a1b2e1c9684193b051aa8671c0ac0a359dd
Ruje Alfon <7766205+rujealfon@users.noreply.github.com>
2025-04-30 22:12:22 +0800

    Add babel-plugin-react-compiler and configure in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
+const ReactCompilerConfig = {};
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+      },
+    }),
+  ],
 });
