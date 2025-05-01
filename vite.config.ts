08c50dc9b5760aaa255411c6e6f43ac1cc258f81
Ruje Alfon <7766205+rujealfon@users.noreply.github.com>
2025-05-01 21:17:18 +0800

    Add Tailwind CSS and update project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 const ReactCompilerConfig = {};
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
   plugins: [
     // TanStack Router plugin must come before react()
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
@@ -14,5 +20,6 @@ export default defineConfig({
         plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
       },
     }),
+    tailwindcss(),
   ],
 });
